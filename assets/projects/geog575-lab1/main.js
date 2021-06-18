0915eb0cf5c0498eb97a6a7464f32c99984345e3
krayf <krayf@northpointgis.com>
2021-06-17 21:43:04 -0400

    full screen map, now shows data

--- assets/projects/geog575-lab1/main.js
+++ assets/projects/geog575-lab1/main.js
@@ -1,6 +1,21 @@
-let lMap = L.map('map').setView([51.505, -0.09], 13);
-
+// set-up map and basemap
+let map = L.map('map').setView([39.47, -97.02], 4);
+let assets = '/assets/projects/geog575-lab1'
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(lMap);
+}).addTo(map);
+
+async function fetchJSON(url) {
+    const response = await fetch(url);
+    return await response.json();
+  }
+
+//  add the data
+fetchJSON(`${assets}/urban_final.json`).then((data) => { 
+    L.geoJSON(data).addTo(map);
+});
+
+fetchJSON(`${assets}/urban_boundaries.json`).then((data) => { 
+    L.geoJSON(data).addTo(map);
+});
