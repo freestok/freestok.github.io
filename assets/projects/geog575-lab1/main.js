3025902eda46092f1ce090da8183d3d96e5aa257
krayf <krayf@northpointgis.com>
2021-06-18 08:11:49 -0400

    updating data and basemap

--- assets/projects/geog575-lab1/main.js
+++ assets/projects/geog575-lab1/main.js
@@ -1,9 +1,9 @@
 // set-up map and basemap
 let map = L.map('map').setView([39.47, -97.02], 4);
 let assets = '/assets/projects/geog575-lab1'
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
 }).addTo(map);
 
 async function fetchJSON(url) {
@@ -19,8 +19,10 @@ function style(feature) {
 
 //  add the data
 //  TODO figure out a better way to do porportions
+// make it specific to the kind (all, owner, renter)
+// have to examine the data in pandas probably
 function getRadius(x) {
-    console.log(x)
+    // console.log(x)
     if (x < .45) return 5
     else if (x < .50) return 10
     else if (x < .55) return 15
@@ -29,12 +31,13 @@ function getRadius(x) {
 }
 
 fetchJSON(`${assets}/urban_final.json`).then((data) => {
+    console.log('data', data);
     L.geoJSON(data, {
         pointToLayer: (feature, latlng) => {
             return L.circleMarker(latlng, {
                 fillColor: '#708598',
                 color: '#537898',
-                radius: getRadius(feature.properties.rp2019),
+                radius: getRadius(feature.properties.ap2019),
                 weight: 1,
                 fillOpacity: 0.6
             }) // TODO add pop-up?
@@ -42,6 +45,7 @@ fetchJSON(`${assets}/urban_final.json`).then((data) => {
     }).addTo(map);
 });
 
+// TODO style this
 // fetchJSON(`${assets}/urban_boundaries.json`).then((data) => {
 //     L.geoJSON(data).addTo(map);
 // });
