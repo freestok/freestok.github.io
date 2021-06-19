b07d56e96e4c89b7072279928f201599d684d68c
krayf <krayf@northpointgis.com>
2021-06-19 09:03:46 -0400

    data can change by housing type, add pop-ups

--- assets/projects/geog575-lab1/main.js
+++ assets/projects/geog575-lab1/main.js
@@ -1,29 +1,69 @@
 // global variables
 let stat = 'rp2013';
+let housing;
+const assets = '/assets/projects/geog575-lab1';
 
-// set-up map and basemap
-let map = L.map('map').setView([39.47, -97.02], 4);
-let assets = '/assets/projects/geog575-lab1'
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
-}).addTo(map);
+$(document).ready(() => {
+    // set-up map and basemap
+    let map = L.map('map').setView([39.47, -97.02], 4);
+
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+        maxZoom: 16
+    }).addTo(map);
+
+    // add data to the map
+    fetchJSON(`${assets}/urban_final.json`).then((data) => {
+        console.log('data', data);
+        housing = L.geoJSON(data, {
+            pointToLayer: (feature, latlng) => {
+                return L.circleMarker(latlng, {
+                    fillColor: '#708598',
+                    color: '#537898',
+                    radius: getRadius(feature.properties[stat]),
+                    weight: 1,
+                    fillOpacity: 0.6
+                }) // TODO add pop-up?
+            }
+        }).addTo(map);
+
+        updateSymbology();
+    });
+
+    // TODO style this
+    // fetchJSON(`${assets}/urban_boundaries.json`).then((data) => {
+    //     L.geoJSON(data).addTo(map);
+    // });
+
+    // ------------------- listeners ------------------------
+    $('.dropdown-item').on('click', (e) => {
+        $('#house-type').html(e.target.innerText);
+        stat = `${e.target.id[0]}p2019`;
+        updateSymbology();
+    });
+});
+
+function updateSymbology() {
+    console.log('updating');
+    housing.eachLayer((layer) => {
+        const props = layer.feature.properties;
+        let popupContent = `
+            ${props.name}<br>
+            ${stat}<br>
+            ${Math.round(props[stat] * 100)}%
+        `
+        layer.bindPopup(popupContent);
+        layer.bindTooltip(popupContent);
+        layer.setRadius(getRadius(props[stat]));
+    });
+}
 
 async function fetchJSON(url) {
     const response = await fetch(url);
     return await response.json();
 }
 
-function style(feature) {
-    return {
-        radius: feature.properties.ap2019 * 10
-    };
-}
-
 //  add the data
-//  TODO figure out a better way to do porportions
-// make it specific to the kind (all, owner, renter)
-// have to examine the data in pandas probably
 // {'ap': {'min': 0.08, 'max': 0.25}, 'op': {'min': 0.16, 'max': 0.48}, 'rp': {'min': 0.41, 'max': 0.64}
 function getRadius(x) {
     if (stat.includes('a')) {
@@ -43,23 +83,3 @@ function getRadius(x) {
         else return 20
     }
 }
-
-fetchJSON(`${assets}/urban_final.json`).then((data) => {
-    console.log('data', data);
-    L.geoJSON(data, {
-        pointToLayer: (feature, latlng) => {
-            return L.circleMarker(latlng, {
-                fillColor: '#708598',
-                color: '#537898',
-                radius: getRadius(feature.properties[stat]),
-                weight: 1,
-                fillOpacity: 0.6
-            }) // TODO add pop-up?
-        }
-    }).addTo(map);
-});
-
-// TODO style this
-// fetchJSON(`${assets}/urban_boundaries.json`).then((data) => {
-//     L.geoJSON(data).addTo(map);
-// });
