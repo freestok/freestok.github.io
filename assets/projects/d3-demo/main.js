c47026c8549b34f4b187514694210e5f56f85e9c
krayf <freestonekray@gmail.com>
2021-07-21 08:54:25 -0400

    got 2016 results by state to show

--- assets/projects/d3-demo/main.js
+++ assets/projects/d3-demo/main.js
@@ -1,3 +1,5 @@
+let year = 2016;
+
 $(document).ready(() => {
     setMap();
     // testThis();
@@ -7,42 +9,71 @@ $(document).ready(() => {
 function setMap() {
     //use queue to parallelize asynchronous data loading
     const files = [
+        '../assets/projects/d3-demo/state16_final.csv',
+        '../assets/projects/d3-demo/state20_final.csv',
         '../assets/projects/d3-demo/election16_modified.csv',
         '../assets/projects/d3-demo/election20_modified.csv',
-        '../assets/projects/d3-demo/usa_election.json'
+        '../assets/projects/d3-demo/counties.json',
+        '../assets/projects/d3-demo/states.json'
     ];
     
 
     //map frame dimensions
+    // TODO combine the state spreadsheets, select by year, avoid if statements
+
     const width = 960,
-        height = 960;
+        height = 425;
 
     //create new svg container for the map
-    let map = d3.select("body")
+    let map = d3.select("div#d3Map")
+        .append("div")
+        .classed("svg-container", true) //container class to make it responsive
         .append("svg")
-        .attr("class", "map")
-        .attr("width", width)
-        .attr("height", height);
+        //responsive SVG needs these 2 attributes and no width and height attr
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", `0 0 ${width} ${height}`)
+        //class to make it responsive
+        .classed("svg-content-responsive", true); 
 
     const promises = [];
+    console.log('loading data..');
     for (let file of files) {
         if (file.includes('.json')) promises.push(d3.json(file));
         else if (file.includes('.csv')) promises.push(d3.csv(file));
     }
 
     Promise.all(promises).then((values) => {
-        let [e16, e17, usaGeom] = values;
-        let usa = topojson.feature(usaGeom, usaGeom.objects.usa_election);
-        const projection = d3.geoAlbersUsa().fitSize([width, height], usa);
+        console.log('data loaded');
+        let [state16, state20, county16, county20, counties, states] = values;
+        console.log('state20',state20);
+        console.log('states',states);
+        let stateTopo = topojson.feature(states, states.objects.usa_election_state);
+        const projection = d3.geoAlbersUsa().fitSize([width, height], stateTopo);
         const path = d3.geoPath().projection(projection);
 
         var regions = map.selectAll(".regions")
-            .data(usa.features)
+            .data(stateTopo.features)
             .enter()
             .append('path')
             .attr('d', path)
-            .style("fill", "red")
+            .style("fill", d => {
+                // console.log('d',d);
+                if (year === 2020) return stateWinners(state20, d.properties.STATEFIP);
+                else return stateWinners(state16, d.properties.STATEFIP);
+            })
             .style("stroke-width", "1")
             .style("stroke", "black");
     });
 };
+
+function stateWinners(csv, fips) {
+    let record = csv.filter(e => e.fips == fips);
+    console.log('record', record);
+
+    if (record.length) {
+        let r = record[0];
+        if (r.dem == '-') return 'red'
+        else return 'blue'
+    }
+    return 'yellow'
+}
