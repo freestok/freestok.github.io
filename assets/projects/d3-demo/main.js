9d6357dd77fe7dd447f2a9de65fc1a8ae6d79e7d
krayf <freestonekray@gmail.com>
2021-07-28 21:04:12 -0400

    got county map to show up, corrected some json/csv data

--- assets/projects/d3-demo/main.js
+++ assets/projects/d3-demo/main.js
@@ -1,4 +1,8 @@
-let year = 2016;
+let year = 2020;
+const blue = '#5d80b4';
+const red = '#c66154';
+const width = 960;
+const height = 425;
 
 $(document).ready(() => {
     setMap();
@@ -16,13 +20,6 @@ function setMap() {
         '../assets/projects/d3-demo/counties.json',
         '../assets/projects/d3-demo/states.json'
     ];
-    
-
-    //map frame dimensions
-    // TODO combine the state spreadsheets, select by year, avoid if statements
-
-    const width = 960,
-        height = 425;
 
     //create new svg container for the map
     let map = d3.select("div#d3Map")
@@ -45,26 +42,72 @@ function setMap() {
     Promise.all(promises).then((values) => {
         console.log('data loaded');
         let [state16, state20, county16, county20, counties, states] = values;
-        console.log('state20',state20);
-        console.log('states',states);
-        let stateTopo = topojson.feature(states, states.objects.usa_election_state);
-        const projection = d3.geoAlbersUsa().fitSize([width, height], stateTopo);
-        const path = d3.geoPath().projection(projection);
 
-        var regions = map.selectAll(".regions")
-            .data(stateTopo.features)
-            .enter()
-            .append('path')
-            .attr('d', path)
-            .style("fill", d => {
-                // console.log('d',d);
-                if (year === 2020) return stateWinners(state20, d.properties.STATEFIP);
-                else return stateWinners(state16, d.properties.STATEFIP);
-            })
-            .style("stroke-width", "1")
-            .style("stroke", "black");
+        // createStateMap(map, states, state20, state16);
+        createCountyMap(map, counties, county16, county20);
     });
 };
+
+
+// --------------------------------------------------------
+// map creation functions ---------------------------------
+// --------------------------------------------------------
+function createCountyMap(map, counties, county16, county20) {
+    console.log('counties', counties.objects.usa_election);
+    console.log('county20', county20);
+    let countyTopo = topojson.feature(counties, counties.objects.usa_election);
+    const projection = d3.geoAlbersUsa().fitSize([width, height], countyTopo);
+    const path = d3.geoPath().projection(projection);
+
+    map.selectAll('.counties')
+        .data(countyTopo.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .style('fill', d => {
+            const fips = d.properties.GEOID;
+            if (year === 2020) return countyChoropleth(county20, fips);
+            else return countyChoropleth(county16, fips)
+        })
+        .style('stroke-width', '0.5')
+        .style('stroke', 'black');
+}
+
+
+function createStateMap(map, states, state20, state16) {
+    let stateTopo = topojson.feature(states, states.objects.usa_election_state);
+    const projection = d3.geoAlbersUsa().fitSize([width, height], stateTopo);
+    const path = d3.geoPath().projection(projection);
+
+    map.selectAll(".regions")
+        .data(stateTopo.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .style("fill", d => {
+            if (year === 2020) return stateWinners(state20, d.properties.STATEFIP);
+            else return stateWinners(state16, d.properties.STATEFIP);
+        })
+        .style("stroke-width", "1")
+        .style("stroke", "black");
+}
+
+// --------------------------------------------------------
+// styling functions --------------------------------------
+// --------------------------------------------------------
+function countyChoropleth(csv, fips) {
+    // console.log('fips', fips);
+    let record = csv.filter(e => e.county_fips == fips);
+    if (record.length) {
+        let r = record[0];
+        if (Number(r.rep) > Number(r.dem)) return red;
+        else return blue;
+    } else {
+        console.log('fips', fips);
+        return 'black';
+    }
+}
+
 
 function stateWinners(csv, fips) {
     let record = csv.filter(e => e.fips == fips);
@@ -72,20 +115,19 @@ function stateWinners(csv, fips) {
 
     if (record.length) {
         let r = record[0];
-        if (r.state == 'District of Columbia') return 'black'
-        if (r.dem == '-') return 'red'
-        else return 'blue'
+        if (r.dem == '-') return red;
+        else return blue;
     } else {
         let new_fips = fips.split('-')[0];
         record = csv.filter(e => e.fips == new_fips);
         let demWinner = Number(record[0].dem) > Number(record[0].rep);
 
         if (fips.includes('-l')) {
-            if (demWinner) return 'blue';
-            else return 'red';
+            if (demWinner) return blue;
+            else return red;
         } else {
-            if (demWinner) return 'red';
-            else return 'blue';
+            if (demWinner) return red;
+            else return blue;
         }
     }
 }
