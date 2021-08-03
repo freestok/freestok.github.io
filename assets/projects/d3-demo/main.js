93bf4ebf02fd199a6078a4432518369669454840
krayf <freestonekray@gmail.com>
2021-08-03 18:09:01 -0400

    have logic to adjust vote share bar

--- assets/projects/d3-demo/main.js
+++ assets/projects/d3-demo/main.js
@@ -5,11 +5,20 @@ import { legend } from './color-legend.js';
 let year = 2020;
 const blue = '#5d80b4';
 const red = '#c66154';
-
+const demShareNat = 0.514;
+const repShareNat = 0.469;  
+let demShare = demShareNat;
+let repShare = repShareNat;
+const duration = 1000;
 
 $(document).ready(() => {
+    setListeners();
     setMap();
 });
+
+function setListeners() {
+    $('#transitionTest').on('click', () => adjustChart());
+}
 
 //set up choropleth map
 function setMap() {
@@ -62,13 +71,13 @@ function createViz(counties, countyData) {
     createChart();
 }
 
-function createChart(demShare=0.8, repShare=0.2) {
+function createChart() {
     let color = d3.scaleOrdinal(['d','r'],[red,blue]),
         title = 'Vote Share',
         tickSize = 6,
         width = 320,
-        demShareStr = `${demShare * 100}%`,
-        repShareStr = `${repShare * 100}%`,
+        demShareStr = `${(demShare * 100).toFixed(1)}%`,
+        repShareStr = `${(repShare * 100).toFixed(1)}%`,
         height = 44 + tickSize,
         graphHeight = 30,
         marginTop = 18,
@@ -84,31 +93,29 @@ function createChart(demShare=0.8, repShare=0.2) {
         .style("overflow", "visible")
         .style("display", "block");
 
-    let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
-    let x = d3.scaleBand()
-        .domain(color.domain())
-        .rangeRound([marginLeft, width - marginRight]);
-
+    // adjust the bars
     svg.append("g")
         .selectAll("rect")
         .data(color.domain())
         .join("rect")
+        .attr("id", (x) => {
+            console.log('id', x);
+            if (x === 'd') return 'demBar';
+            else return 'repBar';
+        })
         .attr("x", (x) => {
-            console.log('x', x);
             if (x === 'd') return (width * demShare);
             else return -1;
         })
         .attr("y", marginTop)
-        // .attr("width", Math.max(0, x.bandwidth() - 1))
         .attr("width", (e) => {
-            console.log('e', e);
-            console.log(Math.max(0, x.bandwidth() - 1));
             if (e === 'd') return repShare * width;
             else return demShare * width;
         })
         .attr("height", graphHeight)
         .attr("fill", color);
 
+    // add the title
     svg.append("g")
         .attr("transform", `translate(0,${height - marginBottom})`)
         .call(g => g.select(".domain").remove())
@@ -119,10 +126,15 @@ function createChart(demShare=0.8, repShare=0.2) {
             .attr("text-anchor", "start")
             .attr("font-weight", "bold")
             .text(title));
-    svg.selectAll("g") // show percentages
+
+    // add the percentages
+    svg.selectAll("g") 
         .append("text")
+        .attr("id", (x,i) => {
+            if (i === 0) return 'demTxt';
+            else return 'repTxt';
+        })
         .attr("x", (x, i) => {
-            console.log('x', i);
             if (i === 0) return (width * demShare)/2;
             else return (width * demShare) + ((width * repShare)/2);
         })
@@ -140,8 +152,44 @@ function createChart(demShare=0.8, repShare=0.2) {
         });
 }
 
-function adjustChart(demShare, repShare) {
-    const svg = d3.select('svg#d3Chart');
+function adjustChart(name, dems, reps) {
+    // let demShare = 0.2,
+    //     repShare = 0.8;
+    const width = 320;
+    
+    d3.select('rect#demBar')
+        .transition()
+        .duration(duration)
+        .attr('x', width * dems)
+        .attr('width', reps * width);
+
+    d3.select('rect#repBar')
+        .transition()
+        .duration(duration)
+        .attr('x', -1)
+        .attr('width',dems * width);
+
+            // add the percentages
+
+    d3.select('text#demTxt')
+        .transition()
+        .duration(duration)
+        .attr("x", (width * dems) / 2)
+        .textTween(() =>{
+            const i = d3.interpolate(demShare, dems);
+            return (t) => `${(i(t) * 100).toFixed(1)}%`;
+        });
+    d3.select('text#repTxt')
+        .transition()
+        .duration(duration)
+        .attr("x", (width * dems) + ((width * reps)/2))
+        .textTween(() =>{
+            const i = d3.interpolate(repShare, reps);
+            return (t) => `${(i(t) * 100).toFixed(1)}%`;
+        });
+
+    demShare = dems;
+    repShare = reps;
 }
 
 function createCountyMap(counties, countyData, divergingScheme) {
@@ -160,6 +208,7 @@ function createCountyMap(counties, countyData, divergingScheme) {
 
     // create map
     const countyTopo = topojson.feature(counties, counties.objects.usa_election);
+    console.log('countyTopo',countyTopo);
     const projection = d3.geoAlbersUsa().fitSize([width, height], countyTopo);
     const path = d3.geoPath().projection(projection);
     
@@ -170,10 +219,20 @@ function createCountyMap(counties, countyData, divergingScheme) {
         .attr('d', path)
         .style('fill', d => countyChoropleth(countyData, d.properties.GEOID, divergingScheme))
         .style('stroke-width', '0.5')
-        .style('stroke', 'black');
-        // .on('mouseover', d => console.log('over!',d))
+        .style('stroke', 'black')
+        .on('mouseover', (event, d) => mouseOver(d.properties.GEOID, countyData));
         // .on('mouseout', () => console.log('out!'));
 }
+
+
+function mouseOver(fips, csv) {
+    let r = csv.filter(e => e.county_fips === fips)[0];
+    let name = r.county_name;
+    let dems = r.dem / r.total;
+    let reps = r.rep / r.total;
+    adjustChart(name, dems, reps);
+}
+
 
 function createStateMap(map, states, state20, state16) {
     const width = 960;
