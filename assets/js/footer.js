3cf41491bf7339d783ae673d80b23026c7523c0b
krayf <freestonekray@gmail.com>
2022-12-13 21:29:12 -0500

    changing to quarto

--- assets/js/footer.js
+++
@@ -1,8 +0,0 @@
-$('img').click((event) => {
-    // console.log(event);
-    if (event.target.className !== 'portfolio-cropper') {
-        // console.log(event.target.className)
-        let url = event.target.currentSrc;
-        window.open(url, 'imgWindow');
-    }
-})
